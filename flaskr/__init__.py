618658cb2c7f66d9aa82cc87c05490c150b5b45a
Woo Pak Yi <woopakyi1@gmail.com>
2026-04-02 17:50:37 +0800

    set for Render

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
